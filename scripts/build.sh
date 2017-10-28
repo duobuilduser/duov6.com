beb56caf163a4735f278627840ceff68bf263d53
duobuilduser <duobuilduser@duosoftware.com>
2017-10-28 14:37:32 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
+
+#sleep 1
+
 BuildOutPath="$PWD"
+username="duobuilduser"
+emailaddress="duobuilduser@duosoftware.com"
+password="DuoS12345"
+
+mkdir "bin" 
+mkdir "pkg" 
+mkdir "src" 
+
 export PATH=$PATH:/usr/local/go/bin;
 export GOPATH=$PWD;
 export PATH=$PATH:$GOPATH/bin;
-
-echo "$PWD"
-echo "$GOPATH"
-echo "BEGIN duoauth build"
-cd duoauth
-go build > /var/www/html/buildtest/duoauth.txt
-go install
+cd bin
+rm *
 cd ../
-echo "END duoauth build"
-echo "BEGIN objectstore build"
-cd objectstore
-go build > /var/www/html/buildtest/objectstore.txt
-go install
+cd src
+
+git config --global user.name $username
+git config --global user.email $emailaddress
+echo ""
+echo "BEGIN REPO PULL v6engine-deps"
+mkdir "depo"
+cd depo
+git clone https://github.com/DuoSoftware/v6engine-deps
+cd ../
+
+
+
+cd depo/v6engine-deps
+git pull
+cp * -r $GOPATH/src
+cd ../
+cd ../
+echo "END REPO PULL v6engine-deps"
+echo ""
+echo "BEGIN REPO PULL v6engine"
+git clone -b development https://github.com/DuoSoftware/v6engine
+mv v6engine duov6.com
