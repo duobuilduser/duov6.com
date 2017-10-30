1a0bab357d9e57125a7c5fd5299c23cf1fc7db5d
duobuilduser <duobuilduser@duosoftware.com>
2017-10-30 15:54:27 +0530

    Update deploy-prebuild.sh

--- scripts/deploy-prebuild.sh
+++ scripts/deploy-prebuild.sh
@@ -1,57 +1,23 @@
 #!/bin/bash
+cd /go/src
 
-BuildOutPath="$PWD"
-username="duobuilduser"
-emailaddress="duobuilduser@duosoftware.com"
-password="DuoS12345"
+go get github.com/ChimeraCoder/anaconda   
+go get github.com/couchbaselabs/go-couchbase
+go get github.com/couchbaselabs/gocb
+go get github.com/denisenkom/go-mssqldb
+go get github.com/fatih/color
+go get github.com/fatih/structs
+go get github.com/go-sql-driver/mysql
+go get github.com/gocql/gocql
+go get github.com/lib/pq
+go get github.com/mattbaird/elastigo/lib
 
-cd /usr/local
-
-rm -r "pkg"
-
-if [ ! -d "bin" ]; then 
-	mkdir "bin" 
-fi
-if [ ! -d "pkg" ]; then 
-	mkdir "pkg" 
-fi
-if [ ! -d "src" ]; then 
-	mkdir "src" 
-fi
-export PATH=$PATH:/usr/local/go/bin;
-export GOPATH=$PWD;
-export PATH=$PATH:$GOPATH/bin;
-cd bin
-rm *
-cd ../
-cd src
-
-git config --global user.name $username
-git config --global user.email $emailaddress
-echo ""
-echo "BEGIN REPO PULL v6engine-deps"
-if [ ! -d "depo" ]; then
-	mkdir "depo"
-	cd depo
-	git clone https://github.com/DuoSoftware/v6engine-deps
-	cd ../
-fi
-
-
-cd depo/v6engine-deps
-git pull
-cp * -r $GOPATH/src
-cd ../
-cd ../
-echo "END REPO PULL v6engine-deps"
-echo ""
-echo "BEGIN REPO PULL v6engine"
-if [ ! -d "duov6.com" ]; then
-	git clone -b development https://github.com/DuoSoftware/v6engine
-	mv v6engine duov6.com
-fi
-cd duov6.com
-git pull
-git log --pretty=format:"%h%x09%an%x09%ad%x09%s" > /var/www/html/build/history.txt
-#mv v6engine duov6.com
-echo "END REPO PULL v6engine"
+go get github.com/twinj/uuid
+go get github.com/xuyu/goredis
+go get golang.org/x/net/context
+go get golang.org/x/oauth2/google
+go get cloud.google.com/go
+go get cloud.google.com/go/bigtable
+go get cloud.google.com/go/datastore
+go get gopkg.in/mgo.v2
+go get gopkg.in/mgo.v2/bson
